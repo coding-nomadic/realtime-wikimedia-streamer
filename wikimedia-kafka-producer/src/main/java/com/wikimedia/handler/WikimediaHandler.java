83dd6f8aa04a6a2b77d00d33f0e3ee3f5cb8fb29
Tenzin Dawa <tnzngdw@gmail.com>
2023-12-18 00:50:17 -0500

    Update WikimediaHandler.java

--- wikimedia-kafka-producer/src/main/java/com/wikimedia/handler/WikimediaHandler.java
+++ wikimedia-kafka-producer/src/main/java/com/wikimedia/handler/WikimediaHandler.java
@@ -5,12 +5,11 @@ import com.launchdarkly.eventsource.MessageEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 
-
 @Slf4j
 public class WikimediaHandler implements EventHandler {
 
-    private KafkaTemplate<String, String> kafkaTemplate;
-    private String topic;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final String topic;
 
     public WikimediaHandler(KafkaTemplate<String, String> kafkaTemplate, String topic) {
         this.kafkaTemplate = kafkaTemplate;
@@ -19,27 +18,28 @@ public class WikimediaHandler implements EventHandler {
 
     @Override
     public void onOpen() throws Exception {
-
+        // No action needed on open
     }
 
     @Override
     public void onClosed() throws Exception {
-
+        // No action needed on closed
     }
 
     @Override
-    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
-        log.info(String.format("New event data -> %s", messageEvent.getData()));
+    public void onMessage(String event, MessageEvent messageEvent) throws Exception {
+        log.info("New event data -> {}", messageEvent.getData());
         kafkaTemplate.send(topic, messageEvent.getData());
     }
 
     @Override
-    public void onComment(String s) throws Exception {
-
+    public void onComment(String comment) throws Exception {
+        // No action needed on comment
     }
 
     @Override
     public void onError(Throwable throwable) {
-
+        // Log or handle the error as needed
+        log.error("Error in WikimediaHandler", throwable);
     }
 }
