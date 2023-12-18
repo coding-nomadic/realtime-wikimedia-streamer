192970c74ae55fe1b5e270e88468d55f93a396c4
Tenzin Dawa <tnzngdw@gmail.com>
2023-12-18 00:44:30 -0500

    Add files via upload

---
+++ wikimedia-kafka-producer/src/test/java/com/example/wikimediakafkaproducer/WikimediaKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wikimediakafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
