c5d39b9ab7acbb6bdeb3612004231fc72e5ea9b3
Tenzin Dawa <tnzngdw@gmail.com>
2023-12-19 18:24:49 -0500

    Update WikimediaHandler.java

--- wikimedia-kafka-producer/src/main/java/com/wikimedia/handler/WikimediaHandler.java
+++ wikimedia-kafka-producer/src/main/java/com/wikimedia/handler/WikimediaHandler.java
@@ -3,43 +3,61 @@ package com.wikimedia.handler;
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.MessageEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Slf4j
 public class WikimediaHandler implements EventHandler {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
-    private final String topic;
+    private final String kafkaTopic;
 
-    public WikimediaHandler(KafkaTemplate<String, String> kafkaTemplate, String topic) {
+    public WikimediaHandler(KafkaTemplate<String, String> kafkaTemplate, String kafkaTopic) {
         this.kafkaTemplate = kafkaTemplate;
-        this.topic = topic;
+        this.kafkaTopic = kafkaTopic;
     }
 
     @Override
     public void onOpen() throws Exception {
-        // No action needed on open
+
     }
 
     @Override
     public void onClosed() throws Exception {
-        // No action needed on closed
+
     }
 
     @Override
     public void onMessage(String event, MessageEvent messageEvent) throws Exception {
         log.info("New event data -> {}", messageEvent.getData());
-        kafkaTemplate.send(topic, messageEvent.getData());
+        ProducerRecord<String, String> record = new ProducerRecord<>(kafkaTopic, messageEvent.getData());
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(record);
+        future.addCallback(handleSendResult());
     }
 
     @Override
     public void onComment(String comment) throws Exception {
-        // No action needed on comment
     }
 
     @Override
     public void onError(Throwable throwable) {
-        // Log or handle the error as needed
         log.error("Error in WikimediaHandler", throwable);
+    }
+
+    private ListenableFutureCallback<SendResult<String, String>> handleSendResult() {
+        return new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("Message sent successfully: {}", result.getRecordMetadata());
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Error sending message: {}", ex.getMessage());
+            }
+        };
     }
 }
